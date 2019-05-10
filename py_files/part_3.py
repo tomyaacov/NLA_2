89a5519ad39b87d7109ee57a7dead165af4e9c46
tomyaacov <tomyaacov1210@gmail.com>
2019-05-10 21:46:07 +0300

    adding part 3 py

--- py_files/part_3.py
+++ py_files/part_3.py
@@ -1,6 +1,6 @@
 from numpy import matmul, array, dot, copy, transpose, vectorize
 from numpy.linalg import norm
-from utils import is_pos_def
+from py_files.utils import is_pos_def
 import matplotlib.pyplot as plt
 
 
@@ -21,19 +21,19 @@ def steepest_decent(A, b, x_0, max_iter, epsilon):
     return x, array(all_r)
 
 
-if __name__ == '__main__':
-    A = array([
-        [5, 4, 4, -1, 0],
-        [3, 12, 4, -5, -5],
-        [-4, 2, 6, 0, 3],
-        [4, 5, -7, 10, 2],
-        [1, 2, 5, 3, 10]
-    ])
-    b = array([1, 1, 1, 1, 1])
-    x_0 = array([0, 0, 0, 0, 0])
-    x, all_r = steepest_decent(A, b, x_0, 50, 0.00000000001)
-    plt.figure(figsize=(20, 20))
-    plt.semilogy(all_r)
-    plt.savefig("part_3.png")
+A = array([
+    [5, 4, 4, -1, 0],
+    [3, 12, 4, -5, -5],
+    [-4, 2, 6, 0, 3],
+    [4, 5, -7, 10, 2],
+    [1, 2, 5, 3, 10]
+])
+b = array([1, 1, 1, 1, 1])
+x_0 = array([0, 0, 0, 0, 0])
+x, all_r = steepest_decent(A, b, x_0, 50, 0.00000000001)
+plt.figure(figsize=(20, 20))
+plt.semilogy(all_r)
+plt.savefig("myplot3.pdf", bbox_inches="tight")
+print(r"\saveandshowplot{myplot3.pdf}")
 
 
