1351db5d11141799b9560c86726076f60fe15cbf
tomyaacov <tomyaacov1210@gmail.com>
2019-05-10 19:24:18 +0300

    starting part 5

---
+++ py_files/part_5_a.py
@@ -0,0 +1,19 @@
+from numpy import matmul, exp, log, asarray, outer
+
+
+def sigmoid(x):
+    return 1 / (1 + exp(-x))
+
+
+def reg_obj_grad_hes(X, y, w):
+    m = len(y)
+    c_1 = y
+    c_2 = 1-y
+    sig = sigmoid(matmul(X.T,w))
+    obj = -(1/m)*(c_1.T*log(sig)+c_2.T*log(1-sig))
+    grad = (1/m)*matmul(X, sig-c_1)
+    # TODO: maybe outer(sig,(1-sig)) should be replaced to diag(sig*(1-sig))
+    hes = (1/m)*matmul(matmul(X, outer(sig,(1-sig))), X.T)
+    return obj, grad, hes
+
+
---
+++ py_files/test_part_5.py
@@ -0,0 +1,14 @@
+from numpy import matmul, exp, log, asarray, outer
+from numpy.random import rand
+from mnist import MNIST
+
+mndata = MNIST('py_files/data')
+mndata.gz = True
+images, labels = mndata.load_training()
+images = asarray(images)
+labels = asarray(labels)
+X = images[labels<=1][:4].T
+X = X/X.max()
+y = labels[labels<=1][:4].T
+w = rand(X.shape[0])*2-1
+print(reg_obj_grad_hes(X, y, w))
